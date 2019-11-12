7c0d35a6571381c624d17f13a3735fd5bb4c9b98
Helene Harmens <helene.harmens@gmail.com>
2019-11-12 20:32:55 +0100

    Added text in index

--- src/main/java/com/wahine/vippsstats/VippsStatsApplication.java
+++ src/main/java/com/wahine/vippsstats/VippsStatsApplication.java
@@ -2,9 +2,17 @@ package com.wahine.vippsstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class VippsStatsApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello!";
+	}
 
 	public static void main(String[] args) {
 
